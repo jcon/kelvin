f6dd56c901f24dd238ce5c8076cd66a56d8eb183
jcon <james.d.connell@gmail.com>
2009-03-04 15:09:25 -0500

    - removed globals for source, dest, and template dirs

--- kelvin.py
+++ kelvin.py
@@ -22,39 +22,29 @@ ch.setFormatter(formatter)
 
 from datetime import datetime
 
-#from google.appengine.ext.webapp import template
-
 import textile
 import yaml
 
-SOURCE_DIR = os.path.join(DIRNAME, '.site')
-DEST_DIR = os.path.join(DIRNAME, '_site')
-TEMPLATE_DIR = os.path.join(SOURCE_DIR, '_layouts')
+#SOURCE_DIR = os.path.join(DIRNAME, '.site')
+#DEST_DIR = os.path.join(DIRNAME, '_site')
+#TEMPLATE_DIR = os.path.join(SOURCE_DIR, '_layouts')
 
 from django import template
 from django.template import loader
 from django.conf import settings
 
-settings.configure(
-    DEBUG=True, 
-    TEMPLATE_DEBUG=True,
-    TEMPLATE_DIRS = (    
-        TEMPLATE_DIR,
-        ),
-    INSTALLED_APPS = (
-        'django.contrib.markup',
-        )
-    )
 
 class File:
-    def __init__(self, dir, name):
+    def __init__(self, source_dir, dest_dir, dir, name):
+        self.source_dir = source_dir
+        self.dest_dir = dest_dir
         self.dir = dir
         self.name = name
-        self.outdir = os.path.join(DEST_DIR, self.dir)
+        self.outdir = os.path.join(self.dest_dir, self.dir)
         self.outfile = self.name
 
     def open(self):
-        return open(os.path.join(SOURCE_DIR, self.dir, self.name))
+        return open(os.path.join(self.source_dir, self.dir, self.name))
 
     def mkdirs(self):
         if not os.path.exists(self.outdir):
@@ -66,12 +56,12 @@ class File:
 
     def output(self, site):
         outdir = self.mkdirs()
-        source = os.path.join(SOURCE_DIR, self.dir, self.name)
+        source = os.path.join(self.source_dir, self.dir, self.name)
         shutil.copy(source, self.destination()) 
 
 class Page(File):
-    def __init__(self, dir, name):
-        File.__init__(self, dir, name)
+    def __init__(self, source_dir, dest_dir, dir, name):
+        File.__init__(self, source_dir, dest_dir, dir, name)
         self.content = self.open().read()
         self.read_data()
         m = re.match(r'([^.]*)\.([^.]*)$', self.name)
@@ -93,7 +83,7 @@ class Page(File):
 #            if self.layout != 'nil':
             if self.data.has_key('layout'):
                 logger.debug("using layout: %s" % self.layout)
-                tf = open(os.path.join(TEMPLATE_DIR, self.layout))
+                tf = open(os.path.join(site.template_dir, self.layout))
                 s = tf.read()
             else:
                 logger.debug("using file as its own layout: [%s]" % self.body)
@@ -117,26 +107,40 @@ class Page(File):
             raise AttributeError("%s is not found in %s" % (name, type(self)))
 
 class Post(Page):
-    def __init__(self, dir, name):
-        Page.__init__(self, dir, name)
+    def __init__(self, source_dir, dest_dir, dir, name):
+        Page.__init__(self, source_dir, dest_dir, dir, name)
 #        m = re.match(r'^(\d+)-(\d+)-(\d+)-(.*)$', self.name)
         m = re.match(r'^(\d+)-(\d+)-(\d+)-([^.]*).*$', self.name)
         date_string = "%s %s %s" % (m.group(1), m.group(2), m.group(3))
         self.date = datetime.strptime(date_string, "%Y %m %d")
         self.url = "/%s/%s/%s/%s.html" % (m.group(1), m.group(2), m.group(3), m.group(4))
-        self.outdir = os.path.join(DEST_DIR, m.group(1), m.group(2), m.group(3))
+        self.outdir = os.path.join(self.dest_dir, m.group(1), m.group(2), m.group(3))
         self.outfile = "%s.html" % m.group(4)
         
     def topics(self):
         return re.split(r'/', self.dir)[1:]
 
 class Site:
-    def __init__(self):
+    def __init__(self, source_dir, dest_dir):
+        self.source_dir = source_dir
+        self.dest_dir = dest_dir
+        self.template_dir = os.path.join(self.source_dir, '_layouts')
         self.posts = []
         self.pages = []
         self.files = []
         self.items = None
         self.time = datetime.now()
+        settings.configure(
+            DEBUG=True, 
+            TEMPLATE_DEBUG=True,
+            TEMPLATE_DIRS = (    
+                self.template_dir,
+                ),
+            INSTALLED_APPS = (
+                'django.contrib.markup',
+                )
+            )
+
 
     def transform(self):
         self.load_items()
@@ -150,9 +154,9 @@ class Site:
 
         for topic in self.topics.keys():
             logger.info("Creating topic: %s" % topic)
-            with open(os.path.join(TEMPLATE_DIR, "topic.html")) as f:
+            with open(os.path.join(self.template_dir, "topic.html")) as f:
                 t = template.Template(f.read())
-                outdir = os.path.join(DEST_DIR, "category", topic)
+                outdir = os.path.join(self.dest_dir, "category", topic)
                 if not os.path.exists(outdir):
                     os.makedirs(outdir)
                 with open(os.path.join(outdir, "index.html"), "w") as outfile:
@@ -165,7 +169,7 @@ class Site:
 
     def load_items(self):
         self.topics = { }
-        for root, dirs, files in os.walk(SOURCE_DIR):
+        for root, dirs, files in os.walk(self.source_dir):
             basedir = root[6:]
             if re.match('^\.site/.git', root):
                 continue
@@ -176,16 +180,16 @@ class Site:
                 elif re.match(r'^\.site/_layouts', root):
                     continue
                 elif re.match(r'^\.site/_posts', root):
-                    post = Post(basedir, f)
+                    post = Post(self.source_dir, self.dest_dir, basedir, f)
                     self.posts.append(post)
                     for topic in post.topics():
                         if not self.topics.has_key(topic):
                             self.topics[topic] = []
                         self.topics[topic].append(post)
                 elif self.is_page(basedir, f):
-                    self.pages.append(Page(basedir, f))
+                    self.pages.append(Page(self.source_dir, self.dest_dir, basedir, f))
                 else:
-                    self.files.append(File(basedir, f))
+                    self.files.append(File(self.source_dir, self.dest_dir, basedir, f))
 
         def post_cmp(left, right):
             return -1 * cmp(left.date, right.date)
@@ -194,12 +198,15 @@ class Site:
             self.topics[topic].sort(post_cmp)
 
     def is_page(self, dir, name):
-        header = open(os.path.join(SOURCE_DIR, dir, name)).read(3)
+        header = open(os.path.join(self.source_dir, dir, name)).read(3)
         return header == "---"
 
 
 def main():
-    site = Site()
+    SOURCE_DIR = os.path.join(DIRNAME, '.site')
+    DEST_DIR = os.path.join(DIRNAME, '_site')
+
+    site = Site(SOURCE_DIR, DEST_DIR)
     site.transform()
 
 if __name__ == "__main__":
